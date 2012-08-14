66b47e8489f9dad76cd90f2dd7d8fcc8824a7f1f
Joseph Balancio <joseph.balancio@betfair.com>
2012-08-13 21:33:28 -0700

    something starting to work

--- public/js/main.js
+++ public/js/main.js
@@ -10,8 +10,67 @@ require.config({
 });
 
 require([
-  'backbone'
-], function(Backbone) {
+  'backbone', 'underscore', 'jquery'
+], function(Backbone, _, $) {
+  console.log(gapi);
+  var config = {
+    "description": "Google Taskrevolver app",
+    "browserApiKey:": "AIzaSyDobpKrhgDFx7u70dYlYvfR67lVk71bMKQ",
+    "clientSecret": "TA8QSkFcEYZ6X1sCsDtawrw-",
+    "clientID": "548079423497.apps.googleusercontent.com",
+    "JavaScript origins": "http://localhost:3000",
+    "blogPost": "http://googleappsdeveloper.blogspot.com/2011/12/using-new-js-library-to-unlock-power-of.html"
+  },
+  scopes = 'https://www.googleapis.com/auth/tasks',
+  list = [
+    'pay car bill'
+  ];
+
+  console.log(gapi.client);
+  gapi.client.setApiKey(config.browserApiKey);
+  gapi.auth.authorize({
+    client_id: config.clientID,
+    scope: scopes,
+    immediate: false
+  }, authorized);
+
+  function authorized() {
+    console.log(arguments);
+    gapi.client.load('tasks', 'v1', function() {
+      console.log(gapi.client);
+      gapi.client.tasks.tasklists.list().execute(function(response) {
+        var mytasklist;
+        _.each(response.items, function(tasklist) {
+          if (tasklist.title === 'Persistent Tasklist') mytasklist = tasklist;
+        });
+
+        function searchForTasks(tasklist) {
+          _.each(list, function(item) {
+            // something goes here
+
+          });
+
+          console.log(tasklist);
+        }
+
+        if (!mytasklist) {
+          gapi.client.tasks.tasklists.insert({
+            title: 'Persistent Tasklist'
+          }).execute(function(response) {
+            mytasklist = response;
+            searchForTasks(mytasklist);
+          });
+        } else {
+          searchForTasks(mytasklist);
+        }
+
+      });
+    });
+  }
+
+
+
+
 
 });
 
